ca391235925cd9cc7c469308fdd15024b7787046
Casey Price <caseyprice217@gmail.com>
2020-10-07 19:24:12 -0700

    Add Invert Binary Tree solution

---
+++ binary-tree/invert-binary-tree.py
@@ -0,0 +1,48 @@
+"""
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root == None:
+            return None
+        
+        self.invertHelper(root)
+        
+        return root
+        
+    def invertHelper(self, root):
+        if root == None:
+            return
+        
+        root.left, root.right = root.right, root.left
+        
+        self.invertHelper(root.left)
+        self.invertHelper(root.right)
